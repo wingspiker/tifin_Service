0786b56fab18c6889d423d872a72b3f79deba04d
rajeshzikasha2003 <rajesh.zikasha@outlook.com>
2025-02-13 18:54:31 +0530

    user model created

---
+++ models/user.js
@@ -0,0 +1,39 @@
+const {DataTypes} =  require('sequelize');
+const sequelize = require('../config/index');
+
+
+const user = sequelize.define('User',{
+    firstname : {
+        type : DataTypes.STRING,
+        allowNull : false
+    },
+    lastname : {
+        type : DataTypes.STRING,
+        allowNull : false
+    },
+    mobile_no : {
+        type : DataTypes.STRING,
+        allowNull : false ,
+        validate : {
+            isNumeric : true,
+            len : [10,15] //Adjust length as needed
+        },
+    } ,
+    email : {
+        type : DataTypes.STRING,
+        allowNull : false , 
+        unique : true ,
+        validate : {
+            isEmail : true,
+        },
+    },
+    password : {
+        type : DataTypes.STRING,
+        allowNull : false,
+    },
+},{
+    timestamps: true,  // Enables created_at and updated_at
+})
+
+module.exports = user ;
+
